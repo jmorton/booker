53b786fdca34218f9dded3642756a07da3595748
Jon Morton <jon.morton@gmail.com>
2018-06-08 14:22:02 -0500

    Install Vue

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,3 +1,8 @@
 Rails.application.config.content_security_policy do |p|
-  p.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+  if Rails.env.development?
+    p.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035"
+    p.script_src :self, :https, :unsafe_eval
+  else
+    p.script_src :self, :https
+  end
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,8 @@
 const { environment } = require('@rails/webpacker')
+const vue =  require('./loaders/vue')
 const webpack = require('webpack')
 
+environment.loaders.append('vue', vue)
 module.exports = environment
 
 environment.plugins.prepend(
