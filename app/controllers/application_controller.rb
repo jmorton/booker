0120834b0663c0ff752273a60c65efef50ba2d83
Jonathan Morton <jon.morton@gmail.com>
2018-06-26 09:58:14 -0500

    Force SSL in production config.
    Moved forced SSL to config given it is an environment specific setting.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include Exceptions
-  force_ssl if: -> { Rails.env.production? }
 
   def pick
     if session[:user].present?
